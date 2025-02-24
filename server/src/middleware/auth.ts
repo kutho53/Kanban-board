f4f15f7eb3c298abd1ca7f760edbe75af49a0d8b
kutho53 <kurtisthomas53@gmail.com>
2025-02-23 23:20:12 -0500

    auth.ts

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,12 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const token = req.headers['authorization']?.split('')[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET,(err, user) => {
+    if (err) return res.sendStatus(403); //invalid token
+    req.user = user; //save user info
+    next();
+  })
 };
